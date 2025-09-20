c3aacfbc8a6aded3e84d7d8bcba5042032f98ddb
Anthony <tonynarciso2003@gmail.com>
2025-09-20 17:46:38 -0500

    Agregando la solucion del item c

--- Lab1_mmpf.py
+++ Lab1_mmpf.py
@@ -11,10 +11,19 @@ f1_deriv = sp.diff(f1, x, 2) + sp.diff(f1, x)
 f2 = sp.sin(x)
 f2_deriv = 3*sp.diff(f2, x, 2) + 2*sp.diff(f2, x) + 2*f2
 
+# Definiendo la funcion para el item (c)
+f3 = 2*sp.exp(x) * sp.cos(x)
+f3_deriv_parte1 = sp.diff(f3, x) - x*f3
+resultado_final = sp.diff(f3_deriv_parte1, x) + f3_deriv_parte1
+
 # Mostrando los resultados
 print("(D^2 + D)e^(2x) =", f1_deriv)
 print("(3D^2 + 2D + 2)sin(x) =", f2_deriv)
+print("(D+1)(D-x)(2e^x + cos(x)) =", resultado_final)
+
 
 # Imprimiendo algunos resultados numericos
+print("\nresultados numericos:")
 print(f1_deriv.subs(x, 0))
 print(f2_deriv.subs(x, 0))
+print(resultado_final.subs(x, 0))
